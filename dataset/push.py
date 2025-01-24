6481077714819064cbc6995f83c8a099ea485662
RetoeGo <65174989+RetoeGo@users.noreply.github.com>
2025-01-24 16:29:02 +0100

    Update push.py

--- dataset/push.py
+++ dataset/push.py
@@ -1,3 +1,4 @@
+# documentation was used
 from datasets import load_dataset
 dataset = load_dataset("imagefolder", data_dir="./images", drop_labels=True)
 dataset.push_to_hub("Retoe/image8k") #replace with your own dataset name
